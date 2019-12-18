deef2d3dabaecf619107981ed0a8339a4410917a
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-12-18 03:13:16 -0800

    cleanup

--- 07/ex06/test2.js
+++ 07/ex06/test2.js
@@ -2,37 +2,39 @@ const UnholyFactory = require("./UnholyFactory.class");
 const Fighter = require("./Fighter.class");
 
 class CrippledSoldier extends Fighter {
-    constructor() {
-        super("crippled soldier");
-    }
+  constructor() {
+    super("crippled soldier");
+  }
 }
 
 const uf = new UnholyFactory();
 uf.absorb(new CrippledSoldier());
 
 requested_fighters = [
-    "crippled soldier",
-    "crippled soldier",
-    "crippled soldier",
-    "crippled soldier"
+  "crippled soldier",
+  "crippled soldier",
+  "crippled soldier",
+  "crippled soldier"
 ];
 
 actual_fighters = [];
 
 for (const i in requested_fighters) {
-    f = uf.fabricate(requested_fighters[i]);
-    if (f != null) {
-        actual_fighters.push(f);
-    }
+  f = uf.fabricate(requested_fighters[i]);
+  if (f != null) {
+    actual_fighters.push(f);
+  }
 }
 
 targets = ["the Hound", "Tyrion", "Podrick"];
 
 for (const i in actual_fighters) {
-    for (const i in targets) {
-        f.fight(targets[t]);
-    }
+  for (const i in targets) {
+    f.fight(targets[t]);
+  }
 }
+
+actual_fighters[0].fight(targets[0]);
 
 // <?php
 
